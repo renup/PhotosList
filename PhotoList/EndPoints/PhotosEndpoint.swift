36bb7ab0536d8e5b92cc1ef82067cb5a3f593733
Renu Punjabi <rpunjabi12@gmail.com>
2019-04-20 18:07:41 -0700

    initial commit

---
+++ PhotoList/EndPoints/PhotosEndpoint.swift
@@ -0,0 +1,9 @@
+//
+//  PhotosEndpoint.swift
+//  PhotoList
+//
+//  Created by Punjabi, Renu on 4/20/19.
+//  Copyright © 2019 Punjabi, Renu. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoList/ViewControllers/PhotosTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotosTableViewController.swift
+//  PhotoList
+//
+//  Created by Punjabi, Renu on 4/20/19.
+//  Copyright © 2019 Punjabi, Renu. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
