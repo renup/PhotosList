d6452473f1cb3862df4f1c280703f7136402acfa
Renu Punjabi <rpunjabi12@gmail.com>
2019-04-21 17:33:57 -0700

    Added quick nimble for tests

---
+++ PhotoListTests/PhotosAPITest.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosAPITest.swift
+//  PhotoListTests
+//
+//  Created by Renu Punjabi on 4/21/19.
+//  Copyright © 2019 Punjabi, Renu. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+final class PhotosAPI: QuickSpec {
+    
+    override func spec() {
+        super.spec()
+        
+        describe("first test") {
+            it("should pass"){
+                expect(1).to(equal(1))
+            }
+        }
+    }
+    
+}
